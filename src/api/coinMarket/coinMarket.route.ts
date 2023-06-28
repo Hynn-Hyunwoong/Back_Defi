603c1feb440cbf4c6c8c037eb9e9423eb87d19e6
Hynn <choihwoong@me.com>
2023-06-28 16:54:42 +0900

    feat:apiconnect

---
+++ src/api/coinMarket/coinMarket.route.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+import { getCoinPriceController, getCoinCurrencyController } from './coinMarket.controller';
+
+export const router = express.Router();
+
+router.get('/coinPrice', getCoinPriceController);
+router.get('/currency', getCoinCurrencyController);
